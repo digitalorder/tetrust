9dba9079dfd1c966bfe0f826d39d70ee112170bc
Denis Vasilkovskii <denis.vasilkovskii@tado.com>
2020-06-21 18:39:05 +0200

    feature(view): View can show next figure

--- src/view.rs
+++ src/view.rs
@@ -2,7 +2,7 @@ use crate::playfield::Playfield;
 use crate::playfield::WIDTH as WIDTH;
 use crate::playfield::HEIGHT as HEIGHT;
 use crate::playfield::Coords as Coords;
-use crate::figures::figures::Shape as Shape;
+use crate::figures::figures::{Shape, Tetromino, LAYOUT_HEIGHT, LAYOUT_WIDTH};
 use std::io::{stdout, Write};
 use termion::raw::IntoRawMode;
 extern crate termion;
@@ -15,6 +15,7 @@ pub trait View {
     fn show_row(&self, playfield: &Playfield, row: i8) -> Row;
     fn show_playfield(&self, playfield: &Playfield);
     fn show_static(&self);
+    fn show_next(self: &Self, tetro: &Tetromino);
     /* TODO: add rows iterator */
 }
 
@@ -47,21 +48,8 @@ impl View for ConsoleView {
             print!("{}", termion::cursor::Goto(2, 3 + (row as u16)));
             for col in 0..WIDTH {
                 let (shape, is_active) = playfield.shape_at(&Coords{row: HEIGHT - row - 1, col: col as i8});
-                let shift = match is_active {
-                    true => 8,
-                    false => 0,
-                };
-                let color: termion::color::AnsiValue = match shape {
-                    Shape::NoShape => termion::color::AnsiValue(0),
-                    Shape::OShape => termion::color::AnsiValue(1 + shift),
-                    Shape::IShape => termion::color::AnsiValue(2 + shift),
-                    Shape::TShape => termion::color::AnsiValue(3 + shift),
-                    Shape::JShape => termion::color::AnsiValue(4 + shift),
-                    Shape::LShape => termion::color::AnsiValue(5 + shift),
-                    Shape::SShape => termion::color::AnsiValue(6 + shift),
-                    Shape::ZShape => termion::color::AnsiValue(7 + shift),
-                };
-                print!("{}  ", termion::color::Bg(color));
+                let color = convert_to_color(shape, is_active);
+                print!("{}  {}", termion::color::Bg(color), termion::color::Bg(termion::color::Black));
             }
         }
         print!("{}", termion::color::Bg(termion::color::Black));
@@ -71,15 +59,55 @@ impl View for ConsoleView {
     }
 
     fn show_static(self: &Self) {
-        let mut stdout = stdout().into_raw_mode().unwrap();
         print!("{}TETRUST v{}. Move: ⬅️ ⬇️ ➡️ . Rotate: Spacebar. Exit: q or ctrl+c or ctrl+z\n\r",
                termion::cursor::Goto(1, 1), env!("CARGO_PKG_VERSION"));
         print!("┌────────────────────┐\n\r");
         for _ in 0..HEIGHT {
             print!("│                    │\n\r");
         }
-        println!("└────────────────────┘\n\r");
+        print!("└────────────────────┘\n\r");
+        let mut stdout = stdout().into_raw_mode().unwrap();
         stdout.flush().unwrap();
+    }
+
+    fn show_next(self: &Self, tetro: &Tetromino) {
+        const BASE_ROW: u16 = 4;
+        const BASE_COL: u16 = 26;
+        print!("{}", termion::cursor::Goto(BASE_COL, BASE_ROW));
+        print!("┌────────┐");
+        for row in 0..LAYOUT_HEIGHT {
+            print!("{}│", termion::cursor::Goto(BASE_COL, BASE_ROW + (row as u16) + 1));
+            for col in 0..LAYOUT_WIDTH {
+                let shape = match tetro.layout[row as usize][col as usize] {
+                    0 => Shape::NoShape,
+                    _ => tetro.shape,
+                };
+                let color = convert_to_color(shape, false);
+                print!("{}  {}", termion::color::Bg(color), termion::color::Bg(termion::color::Black));
+            }
+            print!("│");
+        }
+        print!("{}└────────┘", termion::cursor::Goto(BASE_COL, BASE_ROW + 5));
+        let mut stdout = stdout().into_raw_mode().unwrap();
+        stdout.flush().unwrap();
+    }
+}
+
+fn convert_to_color(shape: Shape, is_active: bool) -> termion::color::AnsiValue {
+    let shift = match is_active {
+        true => 8,
+        false => 0,
+    };
+
+    match shape {
+        Shape::NoShape => termion::color::AnsiValue(0),
+        Shape::OShape => termion::color::AnsiValue(1 + shift),
+        Shape::IShape => termion::color::AnsiValue(2 + shift),
+        Shape::TShape => termion::color::AnsiValue(3 + shift),
+        Shape::JShape => termion::color::AnsiValue(4 + shift),
+        Shape::LShape => termion::color::AnsiValue(5 + shift),
+        Shape::SShape => termion::color::AnsiValue(6 + shift),
+        Shape::ZShape => termion::color::AnsiValue(7 + shift),
     }
 }
 
