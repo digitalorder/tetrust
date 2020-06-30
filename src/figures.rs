5d4684a1cd249506471901691698de5aeeb72f02
Denis Vasilkovskii <denis.vasilkovskii@tado.com>
2020-06-30 20:57:46 +0200

    feature(figures): Super-rotation for i

--- src/figures.rs
+++ src/figures.rs
@@ -31,6 +31,45 @@ pub mod figures {
         iter_col: i8,
     }
 
+    fn super_rotation_i(layout: &mut Layout) {
+        /* rotate following edge items
+         *   0123
+         * 0 --*-
+         * 1 *---
+         * 2 ---*
+         * 3 -*--
+         */
+        let temp = layout[0][2];
+        layout[0][2] = layout[1][0];
+        layout[1][0] = layout[3][1];
+        layout[3][1] = layout[2][3];
+        layout[2][3] = temp;
+        /* rotate following edge items
+         *   0123
+         * 0 -*--
+         * 1 ---*
+         * 2 *---
+         * 3 --*-
+         */
+        let temp = layout[0][1];
+        layout[0][1] = layout[2][0];
+        layout[2][0] = layout[3][2];
+        layout[3][2] = layout[1][3];
+        layout[1][3] = temp;
+        /* rotate inner pieces
+         *   0123
+         * 0 ----
+         * 1 -**-
+         * 2 -**-
+         * 3 ----
+         */
+        let temp = layout[1][1];
+        layout[1][1] = layout[2][1];
+        layout[2][1] = layout[2][2];
+        layout[2][2] = layout[1][2];
+        layout[1][2] = temp;
+    }
+
     fn rotate_special_i(layout: &mut Layout) {
         /* simply transpose matrix */
         for row in 0..4 {
