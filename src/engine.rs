c94d53453bd7d26ca713a0786d37191731e44f64
Denis Vasilkovskii <denis.vasilkovskii@tado.com>
2020-07-04 16:34:13 +0200

    change(engine): Dedicated locked state with infinite number of lock delay resets

--- src/engine.rs
+++ src/engine.rs
@@ -17,6 +17,7 @@ pub mod engine {
     pub enum State {
         CompletionPhase,
         FallingPhase,
+        LockedPhase,
         GameOver,
     }
 
@@ -26,6 +27,7 @@ pub mod engine {
                 State::FallingPhase => "falling",
                 State::CompletionPhase => "completion",
                 State::GameOver => "gameover",
+                State::LockedPhase => "locked",
             };
 
             write!(f, "{}", result)
@@ -35,6 +37,7 @@ pub mod engine {
     #[derive(Clone, PartialEq)]
     pub enum Event {
         Timeout,
+        Reschedule,
         KeyLeft,
         KeyRight,
         KeyTurn,
@@ -55,6 +58,7 @@ pub mod engine {
                 Event::KeyTurn => "🔁",
                 Event::KeyDrop => "⬆️",
                 Event::KeyHold => "✋",
+                Event::Reschedule => "R",
             };
 
             write!(f, "{}", result)
@@ -99,51 +103,70 @@ pub mod engine {
         }
     }
 
-    fn move_down(game: &mut Game) -> State {
-        let move_success = game.playfield.move_active(playfield::Dir::Down);
+    fn is_user_move(event: Event) -> bool {
+        event == Event::KeyLeft || event == Event::KeyRight || event == Event::KeyTurn || event == Event::KeyDown || event == Event::KeyDrop || event == Event::KeyHold
+    }
 
-        if move_success {
-            State::FallingPhase
-        } else {
-            game.playfield.place_active();
+    fn handle_user_move(game: &mut Game, event: Event) -> State {
+        let (move_success, fall_space) =
+            if event == Event::KeyDown {
+                game.playfield.move_active(playfield::Dir::Down)
+            } else if event == Event::KeyLeft {
+                game.playfield.move_active(playfield::Dir::Left)
+            } else if event == Event::KeyRight {
+                game.playfield.move_active(playfield::Dir::Right)
+            } else if event == Event::KeyTurn {
+                game.playfield.turn_active()
+            } else if event == Event::KeyDrop {
+                while game.playfield.move_active(playfield::Dir::Down) == (true, true) {};
+                game.playfield.move_active(playfield::Dir::Down)
+            } else if event == Event::KeyHold {
+                let active_shape = game.playfield.active_shape();
+
+                match game.next_tetro.swap(active_shape) {
+                    Ok(tetro) => {game.playfield.new_active(tetro);},
+                    _ => /* do nothing */{},
+                };
+                (true, true)
+            } else {
+                (true, true)
+            };
+
+        if !move_success && event == Event::KeyDown {
             State::CompletionPhase
+        } else if move_success && !fall_space {
+            game.score.lock_delay();
+            State::LockedPhase
+        } else {
+            State::FallingPhase
         }
     }
 
-    pub fn calculate_frame(game: &mut Game, event: Event) {
+    pub fn calculate_frame(game: &mut Game, event: Event) -> bool {
+        let mut reschedule = false;
         match game.state {
             State::FallingPhase => {
                 if event == Event::Timeout {
                     if game.score.inc_frame_counter() {
-                        game.state = move_down(game);
+                        game.state = handle_user_move(game, Event::KeyDown);
                     }
-                } else if event == Event::KeyDown {
-                    game.state = move_down(game);
-                } else if event == Event::KeyLeft {
-                    game.playfield.move_active(playfield::Dir::Left);
-                } else if event == Event::KeyRight {
-                    game.playfield.move_active(playfield::Dir::Right);
-                } else if event == Event::KeyTurn {
-                    game.playfield.turn_active();
-                } else if event == Event::KeyDrop {
-                    while game.playfield.move_active(playfield::Dir::Down) {
-                        /* reset frame counter only if there was a drop,
-                         * otherwise holding drop key would effectively
-                         * freeze game
-                         */
-                        game.score.lock_delay();
-                    };
-                } else if event == Event::KeyHold {
-                    let active_shape = game.playfield.active_shape();
-
-                    match game.next_tetro.swap(active_shape) {
-                        Ok(tetro) => {game.playfield.new_active(tetro);},
-                        _ => /* do nothing */{},
-                    };
-                }
+                } else if is_user_move(event.clone()) {
+                    game.state = handle_user_move(game, event);
+               }
             },
-            State::CompletionPhase => {
+            State::LockedPhase => {
                 if event == Event::Timeout {
+                    if game.score.inc_frame_counter() {
+                        game.state = State::CompletionPhase;
+                        reschedule = true;
+                    }
+                } else if is_user_move(event.clone()) {
+                    game.state = handle_user_move(game, event);
+                }
+            }
+            State::CompletionPhase => {
+                if event == Event::Timeout || event == Event::Reschedule {
+                    game.playfield.place_active();
                     let removed = game.playfield.remove_filled();
                     game.score.update(removed);
                     game.state = create_new_tetro(game);
@@ -153,5 +176,6 @@ pub mod engine {
                 /* do nothing for now */
             }
         }
+        reschedule
     }
 }
